89353c343a2a26a2a36618195aa0089037b84c5a
Sebastian Garrido <segasi@gmail.com>
2020-09-24 13:18:07 -0500

    Cargar paquetes, definir setup y tema de gráficas

---
+++ 02_codigo/00_paquetes_setup_tema.R
@@ -0,0 +1,25 @@
+### Paquetes ----
+library(pacman)
+p_load(classInt, lubridate, ggraph, ggwordcloud, igraph, readxl, rvest, scales, syuzhet, textstem, tidytext, tidyverse, tm, topicmodels, udpipe, WriteXLS, xml2)
+
+
+### Setup general ----
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") 
+options(scipen = 9999)
+theme_set(theme_gray())
+
+## Definir tema de gráficas ----
+tema <-  theme_minimal() +
+  theme(text = element_text(family = "Didact Gothic Regular", color = "grey35"),
+        plot.title = element_text(size = 28, face = "bold", margin = margin(10,0,20,0), family = "Trebuchet MS Bold", color = "grey25"),
+        plot.subtitle = element_text(size = 16, face = "bold", colour = "#666666", margin = margin(0, 0, 20, 0), family = "Didact Gothic Regular"),
+        plot.caption = element_text(hjust = 0, size = 15),
+        panel.grid = element_line(linetype = 2), 
+        panel.grid.minor = element_blank(),
+        legend.position = "bottom",
+        legend.title = element_text(size = 16, face = "bold", family = "Trebuchet MS Bold"),
+        legend.text = element_text(size = 14, family = "Didact Gothic Regular"),
+        legend.title.align = 0.5,
+        axis.title = element_text(size = 18, hjust = 1, face = "bold", margin = margin(0,0,0,0), family = "Didact Gothic Regular"),
+        axis.text = element_text(size = 16, face = "bold", family = "Didact Gothic Regular"))
+
